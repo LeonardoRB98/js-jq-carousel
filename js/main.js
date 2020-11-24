ed6b1b6f8485abaf885087cc8dc2115148049dcf
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-11-24 19:28:45 +0100

    aggiunta event keydown

--- js/main.js
+++ js/main.js
@@ -2,19 +2,29 @@
 
 $(document).ready(
   function() {
-
+    // foto successiva
     $('.next').click(
       function() {
         nextPhoto();
       }
-    )
-
+    );
+    // foto precedente
     $('.prev').click(
       function() {
         prevPhoto();
       }
-    )
+    );
 
+    // navigazione foto tramite pulsanti freccia della tastiera
+    $(document).keydown(
+      function() {
+        if (event.which == 37) {
+          prevPhoto();
+        } else if (event.which == 39) {
+          nextPhoto();
+        }
+      }
+    )
   }
 );
 
@@ -52,9 +62,11 @@ function prevPhoto() {
   var activeDot = $('.nav i.active');
   console.log(activeDot);
 
+  // rimuovo classe active da activePhoto e activeDot
   activePhoto.removeClass('active');
   activeDot.removeClass('active');
 
+  // condizione se activePhot ha la classe first assegno la classe active a nav i.last e images img.last
   if (activePhoto.hasClass('first')) {
     $('.nav i.last').addClass('active');
     $('.images img.last').addClass('active');
